a2aef8fde3855db05306c70b9dbd524f77a25eb9
Clark Dorman <cedorman01@gmail.com>
2020-11-20 10:22:32 -0500

    Run ./run.sh rather than mcs_test.py

--- ssh_run_docker_command.sh
+++ ssh_run_docker_command.sh
@@ -9,4 +9,7 @@ set -x
 
 
 # Without mapping and data file (which are not really needed)
-ssh -i ~/.ssh/CED_Consulting_AWS_Key_Pair.pem ubuntu@ec2-52-91-116-180.compute-1.amazonaws.com docker run --privileged 924969863225.dkr.ecr.us-east-1.amazonaws.com/mcs-ai2thor-docker python3 mcs_test.py
+ssh -i ~/.ssh/CED_Consulting_AWS_Key_Pair.pem ec2-user@ec2-54-174-13-122.compute-1.amazonaws.com \
+  docker run --privileged 924969863225.dkr.ecr.us-east-1.amazonaws.com/mcs-ai2thor-docker \
+  ./run.sh
+#   python3 mcs_test.py
