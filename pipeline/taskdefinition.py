9d29a0e06f2343da183873bb14a04ebaff0b6c6d
Clark Dorman <cedorman01@gmail.com>
2020-12-10 09:40:51 -0500

    Fix task definition.  For some reason, have to use full path of script file

--- pipeline/taskdefinition.py
+++ pipeline/taskdefinition.py
@@ -4,7 +4,7 @@ class TaskDefinition:
 
     The command will look something like:
 
-    ssh -i ~/.ssh/pemfile.pem ubuntu@ec2.24342.compute.amazon.com "ENV=XYZ performerscript.sh
+    ssh -i ~/.ssh/pemfile.pem ubuntu@ec2.24342.compute.amazon.com "ENV=XYZ performerscript.sh"
 
     where performerscript.sh runs the code and ENV=XYZ sets some environmental variable that
     performer script understands
@@ -14,4 +14,4 @@ class TaskDefinition:
 
 
 class MITTaskDefinition(TaskDefinition):
-    process_command = ["MCS_CONFIG_FILE=/mcs_config_level1.yaml", "mitscript.sh"]
+    process_command = [ "MCS_CONFIG_FILE=/mcs_config_level2.yaml /home/ubuntu/mitscript.sh"]
