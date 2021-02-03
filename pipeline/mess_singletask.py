b4f8b7af8c693645985e3407732f123093483277
clark dorman <clarkus_dormanus@hotmail.com>
2021-02-03 08:32:40 -0500

    WARNING!   Ignoring the output of the run command, assuming that it was correct.  This is because there is a weird seg fault at the end of the scene run that occurs after things have been copied up, so we do not really care, but it looks like it failed.

--- pipeline/mess_singletask.py
+++ pipeline/mess_singletask.py
@@ -6,15 +6,18 @@ import os.path
 from pipeline import util
 from pipeline.singletask import SingleTask
 
+# Where to find tasks.  Be sure to have '/' at end
+all_tasks_dir = "/home/ubuntu/tasks/passive/all/"
+current_tasks_dir = "/home/ubuntu/mess_original_code/mess_final/tasks/"
+
+# The command that will be run
+run_command = "cd /home/ubuntu/mess_original_code/mess_final/ && ./runall.sh"
 
 class MessSingleTask(SingleTask):
 
     def process(self):
         head, tail = os.path.split(self.json_file_name_fullpath)
         self.log.info(f"---- Starting task with json file: {tail}")
-
-        all_tasks_dir = "/home/ubuntu/tasks/passive/all"
-        current_tasks_dir = "/home/ubuntu/mess_original_code/mess_final/tasks"
 
         # Clear out current tasks
         cmd = "rm -f " + current_tasks_dir + "* || true"
@@ -31,11 +34,11 @@ class MessSingleTask(SingleTask):
             return return_code
 
         # Run the command
-        cmd = "conda activate myenv && cd /home/ubuntu/mess_original_code/mess_final && ./runall.sh"
-        return_code = util.shellRunCommand(self.machine_dns, cmd, self.log)
+        return_code = util.shellRunCommand(self.machine_dns, run_command, self.log)
         if return_code != 0:
-            self.log.warn("Failed on run step")
-            return return_code
+            self.log.warn("Failed on run step, but returning 0 anyway")
+            # return return_code
+            return 0
 
         self.log.info(f"---- Ended task with json file: {tail}  Return_code: {return_code}")
         return return_code
